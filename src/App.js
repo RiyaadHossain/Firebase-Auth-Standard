021cc39d5ee4d20a0064cfdb84fd0c25171cc5e3
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-15 20:32:49 +0600

    Navbar and banner image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './Components/Cart/Cart';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar'
+import SignIn from './Components/SignIn/SignIn';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
     </div>
   );
 }
