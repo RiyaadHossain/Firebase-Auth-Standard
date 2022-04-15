d5f34a4aaeb4813e85d4e4f32f5d2cb4aa67eaa8
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-15 20:54:24 +0600

    Button added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className='banner-container'>
-            
-        </div>
-    );
+  return (
+    <div className="banner-container flex justify-center ">
+      <div>
+        <h1 className="text-5xl font-bold text-orange-500 mt-52">
+          Life is what happens when you're busy making other plans
+        </h1>
+       <div className="text-center mt-10"> <button>Begins</button></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
